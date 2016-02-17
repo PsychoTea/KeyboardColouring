6147b170ab449f1494ab88ff4d1dd49d31743949
PsychoTea <bensparkes8@gmail.com>
2016-02-17 20:19:57 +0000

    First commit

---
+++ RazerChromaTest/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using Corale.Colore.Core;
+using Corale.Colore.Razer.Keyboard;
+
+namespace RazerChromaTest
+{
+    public partial class Form1 : Form
+    {
+        public class KeyValue
+        {
+            public Key keyRz { get; set; }
+            public Keys keySys { get; set; }
+            public Color color { get; set; }
+        }
+
+        Color currentColour = new Color();
+
+        private static KeyValue[] GetKeyList()
+        {
+            return new KeyValue[]
+                {
+                    new KeyValue { keyRz = Key.Num7, keySys = Keys.NumPad7, color = Color.Red },
+                    new KeyValue { keyRz = Key.Num8, keySys = Keys.NumPad8, color = Color.Blue },
+                    new KeyValue { keyRz = Key.Num9, keySys = Keys.NumPad9, color = Color.Green },
+                    new KeyValue { keyRz = Key.Num4, keySys = Keys.NumPad4, color = Color.Orange },
+                    new KeyValue { keyRz = Key.Num5, keySys = Keys.NumPad5, color = Color.Yellow },
+                    new KeyValue { keyRz = Key.Num6, keySys = Keys.NumPad6, color = Color.Pink },
+                    new KeyValue { keyRz = Key.Num1, keySys = Keys.NumPad1, color = Color.HotPink },
+                    new KeyValue { keyRz = Key.Num2, keySys = Keys.NumPad2, color = Color.Purple },
+                    new KeyValue { keyRz = Key.Num3, keySys = Keys.NumPad3, color = Color.White },
+                    new KeyValue { keyRz = Key.Num0, keySys = Keys.NumPad0, color = Color.Black },
+                };
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+            Default();
+        }
+
+        private void Default()
+        {
+            keyboard.SetAll(Color.Black);
+            foreach (KeyValue value in GetKeyList())
+            {
+                keyboard.SetKey(value.keyRz, value.color);
+            }
+            SetColour(currentColour);
+        }
+
+        private IKeyboard keyboard = Keyboard.Instance;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (new Keys[] { Keys.NumPad0, Keys.NumPad1, Keys.NumPad2, Keys.NumPad3, Keys.NumPad4, Keys.NumPad5, Keys.NumPad6, Keys.NumPad7, Keys.NumPad8, Keys.NumPad9, Keys.NumPad0 }.Contains(e.KeyCode))
+            {
+                SetColour(
+                    (from key in GetKeyList()
+                     where key.keySys == e.KeyCode
+                     select key.color).First());
+            }
+
+            if (!(new Keys[] { Keys.NumLock, Keys.Divide, Keys.Multiply, Keys.Subtract, Keys.Add, Keys.Decimal }.Contains(e.KeyCode)))
+            {
+                if (e.KeyCode == Keys.OemQuestion) keyboard.SetPosition(4, 12, currentColour);
+                if (e.KeyCode == Keys.Oem7) keyboard.SetPosition(3, 13, currentColour);
+
+                    if (FullKeyMap.Contains(e.KeyCode)) keyboard.SetKey(FullKeyMap.Get(e.KeyCode), currentColour);
+            };
+
+            if (e.KeyCode == Keys.Enter) Default();
+        }
+
+        void SetColour(Color colour)
+        {
+            currentColour = colour;
+
+            keyboard.SetKeys(currentColour, Key.NumLock, Key.NumDivide, Key.NumMultiply, Key.NumSubtract, Key.NumAdd, Key.NumDecimal);
+        }
+    }
+}
---
+++ RazerChromaTest/KeyMap.cs
@@ -0,0 +1,132 @@
+﻿using ChromaKey = Corale.Colore.Razer.Keyboard.Key;
+using System.Windows.Forms;
+using System.Collections;
+using Corale.Colore.Core;
+using Corale.Colore.Razer.Keyboard;
+
+namespace RazerChromaTest
+{
+    public static class FullKeyMap
+    {
+        private static readonly Hashtable Mapping = new Hashtable()
+        {
+            { Keys.A, ChromaKey.A },
+            { Keys.Add, ChromaKey.NumAdd },
+            { Keys.B, ChromaKey.B },
+            { Keys.Back, ChromaKey.Backspace },
+            { Keys.C, ChromaKey.C },
+            { Keys.CapsLock, ChromaKey.CapsLock },
+            { Keys.D, ChromaKey.D },
+            { Keys.D0, ChromaKey.D0 },
+            { Keys.D1, ChromaKey.D1 },
+            { Keys.D2, ChromaKey.D2 },
+            { Keys.D3, ChromaKey.D3 },
+            { Keys.D4, ChromaKey.D4 },
+            { Keys.D5, ChromaKey.D5 },
+            { Keys.D6, ChromaKey.D6 },
+            { Keys.D7, ChromaKey.D7 },
+            { Keys.D8, ChromaKey.D8 },
+            { Keys.D9, ChromaKey.D9 },
+            { Keys.Decimal, ChromaKey.NumDecimal },
+            { Keys.Delete, ChromaKey.Delete },
+            { Keys.Divide, ChromaKey.NumDivide },
+            { Keys.Down, ChromaKey.Down },
+            { Keys.E, ChromaKey.E },
+            { Keys.End, ChromaKey.End },
+            { Keys.Enter, ChromaKey.Enter },
+            { Keys.Escape, ChromaKey.Escape },
+            { Keys.F, ChromaKey.F },
+            { Keys.F1, ChromaKey.F1 },
+            { Keys.F10, ChromaKey.F10 },
+            { Keys.F11, ChromaKey.F11 },
+            { Keys.F12, ChromaKey.F12 },
+            { Keys.F2, ChromaKey.F2 },
+            { Keys.F3, ChromaKey.F3 },
+            { Keys.F4, ChromaKey.F4 },
+            { Keys.F5, ChromaKey.F5 },
+            { Keys.F6, ChromaKey.F6 },
+            { Keys.F7, ChromaKey.F7 },
+            { Keys.F8, ChromaKey.F8 },
+            { Keys.F9, ChromaKey.F9 },
+            { Keys.G, ChromaKey.G },
+            { Keys.H, ChromaKey.H },
+            { Keys.Home, ChromaKey.Home },
+            { Keys.I, ChromaKey.I },
+            { Keys.Insert, ChromaKey.Insert },
+            { Keys.J, ChromaKey.J },
+            { Keys.K, ChromaKey.K },
+            { Keys.L, ChromaKey.L },
+            { Keys.Left, ChromaKey.Left },
+            { Keys.Alt, ChromaKey.LeftAlt },
+            { Keys.LControlKey, ChromaKey.LeftControl },
+            { Keys.LShiftKey, ChromaKey.LeftShift },
+            { Keys.LWin, ChromaKey.LeftWindows },
+            { Keys.M, ChromaKey.M },
+            { Keys.Multiply, ChromaKey.NumMultiply },
+            { Keys.N, ChromaKey.N },
+            { Keys.None, ChromaKey.Invalid },
+            { Keys.NumLock, ChromaKey.NumLock },
+            { Keys.NumPad0, ChromaKey.Num0 },
+            { Keys.NumPad1, ChromaKey.Num1 },
+            { Keys.NumPad2, ChromaKey.Num2 },
+            { Keys.NumPad3, ChromaKey.Num3 },
+            { Keys.NumPad4, ChromaKey.Num4 },
+            { Keys.NumPad5, ChromaKey.Num5 },
+            { Keys.NumPad6, ChromaKey.Num6 },
+            { Keys.NumPad7, ChromaKey.Num7 },
+            { Keys.NumPad8, ChromaKey.Num8 },
+            { Keys.NumPad9, ChromaKey.Num9 },
+            { Keys.O, ChromaKey.O },
+            { Keys.OemBackslash, ChromaKey.OemBackslash },
+            { Keys.OemCloseBrackets, ChromaKey.OemRightBracket },
+            { Keys.Oemcomma, ChromaKey.OemComma },
+            { Keys.OemMinus, ChromaKey.OemMinus },
+            { Keys.OemOpenBrackets, ChromaKey.OemLeftBracket },
+            { Keys.OemPeriod, ChromaKey.OemPeriod },
+            { Keys.OemSemicolon, ChromaKey.OemSemicolon },
+            { Keys.P, ChromaKey.P },
+            { Keys.PageDown, ChromaKey.PageDown },
+            { Keys.PageUp, ChromaKey.PageUp },
+            { Keys.Pause, ChromaKey.Pause },
+            { Keys.Print, ChromaKey.PrintScreen },
+            { Keys.PrintScreen, ChromaKey.PrintScreen },
+            { Keys.Q, ChromaKey.Q },
+            { Keys.R, ChromaKey.R },
+            { Keys.Right, ChromaKey.Right },
+            { Keys.RControlKey, ChromaKey.RightControl },
+            { Keys.RShiftKey, ChromaKey.RightShift },
+            { Keys.S, ChromaKey.S },
+            { Keys.Scroll, ChromaKey.Scroll },
+            { Keys.Space, ChromaKey.Space },
+            { Keys.Subtract, ChromaKey.NumSubtract },
+            { Keys.T, ChromaKey.T },
+            { Keys.Tab, ChromaKey.Tab },
+            { Keys.U, ChromaKey.U },
+            { Keys.Up, ChromaKey.Up },
+            { Keys.V, ChromaKey.V },
+            { Keys.W, ChromaKey.W },
+            { Keys.X, ChromaKey.X },
+            { Keys.Y, ChromaKey.Y },
+            { Keys.Z, ChromaKey.Z },
+            
+            { Keys.Menu, ChromaKey.RightAlt },
+            { Keys.Oem5, ChromaKey.EurBackslash },
+            { Keys.Apps, ChromaKey.RightMenu },
+            { Keys.Oemplus, ChromaKey.OemEquals },
+
+            { Keys.Oemtilde, ChromaKey.OemApostrophe },
+            { Keys.Oem8, ChromaKey.OemTilde },
+            { Keys.OemQuestion, ChromaKey.OemSlash },
+        };
+
+        public static bool Contains(Keys key)
+        {
+            return Mapping.ContainsKey(key);
+        }
+
+        public static ChromaKey Get(Keys keys)
+        {
+            return (ChromaKey)Mapping[keys];
+        }
+    }
+}
